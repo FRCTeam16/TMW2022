8ac15bcf898e5731ee3f3c571e5f6e453c59dbac
FRC Team 16 <frcteam16@gmail.com>
2022-02-26 01:48:56 -0600

    Just a start to the 5 Ball auto

--- src/main/java/frc/robot/auto/strategies/FiveBallStragety.java
+++ src/main/java/frc/robot/auto/strategies/FiveBallStragety.java
@@ -6,14 +6,55 @@ package frc.robot.auto.strategies;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Subsystems;
+import frc.robot.commands.SimpleDistanceDriveCommand;
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class FiveBallStragety extends SequentialCommandGroup {
   /** Creates a new FiveBallStragety. */
   public FiveBallStragety() {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    
+    addCommands(
+      initialState(),
+      pickupFirstBall(),
+      //shootFirstLoad(),
+      pickupSecondBall()
+
+      //
+    );
+    
+  }
+
+  private Command initialState() {
+    return CommandGroupBase.parallel(
+      new InstantCommand(Subsystems.drivetrainSubsystem::zeroGyroscope),
+      new InstantCommand(() -> Subsystems.drivetrainSubsystem.resetOdometry(new Pose2d(0, 0, new Rotation2d())))
+    );
+  }
+  private Command pickupFirstBall() {
+    return CommandGroupBase.sequence(
+      CommandGroupBase.parallel(
+        new InstantCommand(Subsystems.intakeSubsystem::DropIntake),
+        new InstantCommand(Subsystems.intakeSubsystem::enable)),
+      new SimpleDistanceDriveCommand(30, 0.5, -1, -0.25)
+    );
+  }
+  private Command pickupSecondBall(){
+    return CommandGroupBase.sequence(
+      CommandGroupBase.parallel(
+        new InstantCommand(Subsystems.intakeSubsystem::enable)),
+      new SimpleDistanceDriveCommand(30, 0.5, -1, -0.25)
+      
+    );
   }
 }
