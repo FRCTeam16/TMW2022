a9e1de5bc2db3afc2adb1fb6f5bc96cbef53cce5
FRC Team 16 <frcteam16@gmail.com>
2022-03-19 15:53:44 -0500

    adjust 5 ball end move

--- src/main/java/frc/robot/auto/strategies/FiveBallStrategyPartDeux.java
+++ src/main/java/frc/robot/auto/strategies/FiveBallStrategyPartDeux.java
@@ -128,7 +128,7 @@ public class FiveBallStrategyPartDeux extends SequentialCommandGroup {
     double robotAngle = -170.0;
     double speed = 1.0;
     double distX = -4.25;  // -4.5
-    double distY = -0.25;  // -1.0
+    double distY = -0.5;  // -1.0
 
     return CommandGroupBase.sequence(
       // new ProfiledDistanceDriveCommand(robotAngle, speed, 0, -0.1).withTimeout(0.5),
