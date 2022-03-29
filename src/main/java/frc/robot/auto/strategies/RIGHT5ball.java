6f0812a6b1a93cb72f1eaa12b088bbfd4a857054
FRC Team 16 <frcteam16@gmail.com>
2022-03-28 22:24:57 -0500

    Added left and right field auto for the comp

---
+++ src/main/java/frc/robot/auto/strategies/RIGHT5ball.java
@@ -0,0 +1,150 @@
+package frc.robot.auto.strategies;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Subsystems;
+import frc.robot.commands.auto.InitializeAutoState;
+import frc.robot.commands.testing.ProfiledDistanceDriveCommand;
+import frc.robot.commands.vision.TrackVisionTargetWithTurretCommand;
+import frc.robot.subsystems.ShooterSubsystem.ShooterProfile;
+
+public class RIGHT5ball extends SequentialCommandGroup {
+  /** Creates a new FiveBallStragety. */
+  public RIGHT5ball() {
+
+    addCommands(
+        new InitializeAutoState(-90.0, ShooterProfile.Short),
+        shootFirstBall(1.3),
+        pickupFirstBallAndReturn(),
+        //pickupIntakeAndMoveBack(),
+        //shootLoad(2.0),
+        // pickupSecondBall(),
+        pickupSecondBallAngle(),  
+        shootLoad(2.0),
+        pickupThirdBall(),
+        finishAuto()
+        );
+  }
+
+  private Command shootFirstBall(double shootTime) {
+    return CommandGroupBase.sequence(
+      new InstantCommand(() -> System.out.println("**** shootLoad: " + DriverStation.getMatchTime())),
+      new TrackVisionTargetWithTurretCommand().withTimeout(0.25),
+      new WaitCommand(1),
+      new InstantCommand(() -> Subsystems.feederSubsystem.pull(true)),
+      new WaitCommand(shootTime), // maybe don't pull if the wrong direction
+      new InstantCommand(Subsystems.feederSubsystem::dontPull)
+      );
+  }
+  private Command pickupFirstBallAndReturn() {
+    double robotAngle = -90.0;
+    double speed = 1.0;
+    double driveX = 0;
+    double driveY = -1.4;
+
+    return CommandGroupBase.sequence(
+        CommandGroupBase.parallel(
+           new InstantCommand(() -> Subsystems.feederSubsystem.pull(false)),
+            new InstantCommand(Subsystems.intakeSubsystem::enable),
+            new ProfiledDistanceDriveCommand(robotAngle, speed, driveX, driveY)
+              .withEndSpeed(0.2)
+              .withThreshold(0.02)
+              .withTimeout(1.0)
+        ),
+        new InstantCommand(() -> System.out.println("**** after move: " + DriverStation.getMatchTime())),
+        stop(robotAngle).withTimeout(0.1),
+        new InstantCommand(() -> System.out.println("**** after stop: " + DriverStation.getMatchTime()))
+        // new WaitCommand(0.1),
+        /*
+        new ProfiledDistanceDriveCommand(robotAngle, speed, -driveX, -driveY).withThreshold(0.03).withTimeout(2.0),
+        stop(robotAngle)
+        */
+        
+    );
+  }
+
+  private Command shootLoad(double shootTime) {
+    return CommandGroupBase.sequence(
+        new InstantCommand(() -> System.out.println("**** shootLoad: " + DriverStation.getMatchTime())),
+        new TrackVisionTargetWithTurretCommand().withTimeout(0.25),
+        new InstantCommand(() -> Subsystems.feederSubsystem.pull(true)),
+        new WaitCommand(shootTime), // maybe don't pull if the wrong direction
+        new InstantCommand(Subsystems.feederSubsystem::dontPull)
+    );
+  }
+  
+
+  
+  private Command pickupSecondBall() {
+    double robotAngle = -180.0;
+    double speed = 0.5;
+    double distX = -2.95;
+    double distY = 0.0;
+
+    return CommandGroupBase.sequence(
+      new InstantCommand(() -> System.out.println("==> pickupSecondBall")),
+      new InstantCommand(() -> Subsystems.shooterSubsystem.setProfile(ShooterProfile.Dynamic)),
+      new ProfiledDistanceDriveCommand(robotAngle, speed, distX, distY),
+      stop(robotAngle)
+    );
+  }
+
+  private Command pickupSecondBallAngle() {
+    double robotAngle = 140.0;
+    double speed = 1.0;
+    double distX = -3.3;
+    double distY = 1.2;
+
+    return CommandGroupBase.sequence(
+      CommandGroupBase.parallel(
+        new InstantCommand(() -> System.out.println("==> pickupSecondBall")),
+        new InstantCommand(Subsystems.intakeSubsystem::enable),
+        new InstantCommand(() -> Subsystems.shooterSubsystem.setProfile(ShooterProfile.Dynamic))
+      ),
+      new ProfiledDistanceDriveCommand(robotAngle, speed, distX, distY).withEndSpeed(0.2).withTimeout(3),
+      stop(robotAngle),
+      // Turn to next
+      new ProfiledDistanceDriveCommand(180.0, 0.2, -0.25, -0.25).withTimeout(0.5),
+      stop(180.0)
+    );
+  }
+
+
+  // hypotenuce to the tarmac ball is 4.2 meters
+  private Command pickupThirdBall() {
+    double robotAngle = -170.0;
+    double speed = 1.0;
+    double distX = -4.25;  // -4.5
+    double distY = -0.5;  // -1.0
+
+    return CommandGroupBase.sequence(
+      // new ProfiledDistanceDriveCommand(robotAngle, speed, 0, -0.1).withTimeout(0.5),
+      // new ProfiledDistanceDriveCommand(robotAngle, 0, 0, 0).withTimeout(0.25),
+      
+      new ProfiledDistanceDriveCommand(robotAngle, speed, distX, distY).withEndSpeed(0.2).withThreshold(0.03),
+      stop(robotAngle),
+      new WaitCommand(0.5),
+      new ProfiledDistanceDriveCommand(robotAngle, speed, -(distX), -distY).withEndSpeed(0.2)
+    );
+  }
+
+   /*
+   * finishAuto() is so we can have the robot sit at the tarmac for the human
+   * player to feed a ball to if we want it to until auto is complete
+   */
+  private Command finishAuto() {
+    return CommandGroupBase.sequence(
+        CommandGroupBase.parallel(
+            new InstantCommand(Subsystems.intakeSubsystem::enable),
+            new InstantCommand(() -> Subsystems.feederSubsystem.pull(true))));
+  }
+
+  private Command stop(double robotAngle) {
+    return new ProfiledDistanceDriveCommand(robotAngle, 0, 0, 0).withTimeout(0.1);
+  }
+
+}
